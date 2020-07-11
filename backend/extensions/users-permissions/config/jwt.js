419247261c52c08ca901629e6d41beb5b1cca311
DavidddM <davit.maisuradze.1@btu.edu.ge>
2020-07-11 11:07:03 +0400

    initial commit

--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '5b8904f1-5033-4854-b6f1-9d681bdcda6f'
-};
--- frontend/src/settings.js
+++
@@ -1,5 +0,0 @@
-const settings = {
-    APP_BASE_URL: "http://localhost:1337",
-};
-
-export default settings;
